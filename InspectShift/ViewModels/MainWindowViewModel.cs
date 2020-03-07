746375b92780a20721a35b692720efe0ccad64a2
hjyanghj <hjyanghj@gmail.com>
2020-03-07 10:01:14 +0800

    Add  Project InspectShoft. 檢查定位效果如何

---
+++ InspectShift/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Mvvm;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace InspectShift.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        private string[] files;
+        public string[] Files
+        {
+            get { return files; }
+            set { SetProperty(ref files, value); }
+        }
+        private BitmapSource bitmapSrc;
+        public BitmapSource BitmapSrc
+        {
+            get { return bitmapSrc; }
+            set { SetProperty(ref bitmapSrc, value); }
+        }
+        private string selectedFile;
+        public string SelectedFile
+        {
+            get { return selectedFile; }
+            set { 
+                SetProperty(ref selectedFile, value); 
+                if (File.Exists(selectedFile))
+                {
+                    BitmapSrc = new BitmapImage(new Uri(selectedFile));
+                }
+            }
+        }
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
---
+++ InspectShift/Views/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using InspectShift.ViewModels;
+using System.IO;
+using System.Windows;
+
+namespace InspectShift.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = DataContext as MainWindowViewModel;
+        }
+
+        private void btnPickDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            //if (tbxDirectory.Text == "" || !Directory.Exists(tbxDirectory.Text))
+            //    return;
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbxDirectory.Text = dialog.SelectedPath;
+                vm.Files = Directory.GetFiles(tbxDirectory.Text, tbxPattern.Text, SearchOption.TopDirectoryOnly);
+            }
+        }
+    }
+}
